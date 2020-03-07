805eb8ab9792f3c35e77b343a225bbedef34620f
Serge Modin <modinsv@yandex.ru>
2020-03-07 15:23:00 +0300

    Basic Start
    Basic setup Pages Routes

---
+++ src/places/pages/NewPlace.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const NewPlace = props => {
+  return <h2>New Places</h2>;
+};
+
+export default NewPlace;
---
+++ src/user/pages/Users.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Users = () => {
+  return <h2>It works!! Users here!!!</h2>;
+};
+
+export default Users;
