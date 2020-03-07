629c0001e0b155b14d3530617ff74d09edaacb08
Serge Modin <modinsv@yandex.ru>
2020-03-07 16:58:06 +0300

    Styling App Adding some Components

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,7 +1,20 @@
 import React from "react";
 
+import UsersList from '../components/UsersList'
+
 const Users = () => {
-  return <h2>It works!! Users here!!!</h2>;
+
+    const USERS = [
+      {
+        id: "u1",
+        name: "Some Chick  ",
+        image:
+          "https://sf-cdn.fra1.cdn.digitaloceanspaces.com/357650/conversions/ruby-modine_0-medium.jpg",
+        places: 3
+      }
+    ];
+
+  return <UsersList items={USERS}/>;
 };
 
 export default Users;
