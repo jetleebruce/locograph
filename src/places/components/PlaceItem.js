550c8ac1456af761ecd5caa66cc407b098375494
Serge Modin <modinsv@yandex.ru>
2020-03-08 14:37:09 +0300

    Customizing Buttons

--- src/places/components/PlaceItem.js
+++ src/places/components/PlaceItem.js
@@ -2,6 +2,7 @@ import React from "react";
 
 import Card from "../../shared/components/UIElements/Card";
 import "./PlaceItem.css";
+import Button from "../../shared/components/FormElements/Button";
 
 const PlaceItem = props => {
   return (
@@ -16,9 +17,9 @@ const PlaceItem = props => {
           <p>{props.description}</p>
         </div>
         <div className='place-item__actions'>
-          <button>View on Map</button>
-          <button>EDIT</button>
-          <button>DELETE</button>
+          <Button inverse>View on Map</Button>
+          <Button to="{`/places/${props.id}`}">EDIT</Button>
+          <Button danger>DELETE</Button>
         </div>
       </Card>
     </li>
