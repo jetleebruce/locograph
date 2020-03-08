d444e367fc2435801f24ce1f40f890fec74a2b73
Serge Modin <modinsv@yandex.ru>
2020-03-08 23:55:21 +0300

    Rendering the Map With Google Maps

--- src/places/components/PlaceItem.js
+++ src/places/components/PlaceItem.js
@@ -4,6 +4,7 @@ import Card from "../../shared/components/UIElements/Card";
 import "./PlaceItem.css";
 import Button from "../../shared/components/FormElements/Button";
 import Modal from "../../shared/components/UIElements/Modal";
+import Map from "../../shared/components/UIElements/Map"
 
 const PlaceItem = props => {
   const [showMap, setShowMap] = useState(false);
@@ -21,7 +22,7 @@ const PlaceItem = props => {
         footer={<Button onClick={closeMapHandler}>CLOSE</Button>}
       >
         <div className="map-container">
-          <h2>THE MAP!! </h2>
+          <Map center={props.coordinates} zoom={16}/>
         </div>
       </Modal>
       <li className='place-item'>
