22bdfacb110fbd0bb2781e41c959e4149c7e4f7f
Serge Modin <modinsv@yandex.ru>
2020-04-04 07:39:08 +0300

    Custom Form Component Added

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,7 +1,12 @@
 import React from "react";
 
+import './NewPlace.css'
+import Input from '../../shared/components/FormElements/Input'
+
 const NewPlace = props => {
-  return <h2>New Places</h2>;
+  return( <form className="place-form">
+     <Input element='input' type="text" label="Title" /> 
+  </form>);
 };
 
 export default NewPlace;
