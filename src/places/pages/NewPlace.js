9eb3a547ccf98e4de7fae62e91b82f29608a3503
Serge Modin <modinsv@yandex.ru>
2020-04-04 09:45:10 +0300

    Managing state in Input component

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -5,7 +5,7 @@ import Input from '../../shared/components/FormElements/Input'
 
 const NewPlace = props => {
   return( <form className="place-form">
-     <Input element='input' type="text" label="Title" /> 
+     <Input element='input' type="text" label="Here is a place" validators={[]} errorText="Please, enter a valid title"/> 
   </form>);
 };
 
