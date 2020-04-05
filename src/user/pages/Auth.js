08158905cb7da64bab5147cc4c50eed361544823
Serge Modin <modinsv@yandex.ru>
2020-04-05 10:13:49 +0300

    SignUp & Switch Mode Button Added

--- src/user/pages/Auth.js
+++ src/user/pages/Auth.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 import "../../places/pages/PlaceForm.css";
 import './Auth.css'
@@ -7,14 +7,17 @@ import Card from '../../shared/components/UIElements/Card'
 import {
     
     VALIDATOR_MINLENGTH,
-    VALIDATOR_EMAIL
+    VALIDATOR_EMAIL,
+    VALIDATOR_REQUIRE
   } from "../../shared/util/validators";
   import { useForm } from "../../shared/hooks/form-hook";
   import Input from "../../shared/components/FormElements/Input";
   import Button from "../../shared/components/FormElements/Button";
 
 const Auth = () => {
-    const [formState, inputHandler] = useForm({
+    const [isLoginMode, setIsLoginMode] = useState(true);
+
+    const [formState, inputHandler, setFormData] = useForm({
         email: {
           value: "",
           isValid: false,
@@ -26,6 +29,15 @@ const Auth = () => {
         
       });
 
+      const switchModeHandler = () => {
+        if (!isLoginMode) {
+          setFormData({...formState.inputs, name: undefined}, formState.inputs.email.isValid && formState.inputs.password.isValid)
+        } else {
+          setFormData({...formState.inputs, name: {value: '', isValid: false}}, false)
+        }
+        setIsLoginMode(prevMode => !prevMode)
+      }
+
       const authSubmitHandler = (event) => {
         event.preventDefault();
         console.log(formState.inputs); //send this to backend
@@ -36,6 +48,18 @@ const Auth = () => {
       <h2>Login Required</h2>
       <hr/>
       <form  onSubmit={authSubmitHandler}> 
+
+      {!isLoginMode && (
+        <Input 
+        id="name" 
+        element="input" 
+        type="text" 
+        label="Your name" 
+        validators={[VALIDATOR_REQUIRE()]} 
+        errorText="Please enter your name" 
+        onInput={inputHandler}
+        />
+      )}
    
     <Input
     id="email"
@@ -57,8 +81,10 @@ const Auth = () => {
     errorText='Please, enter a valid password'
     onInput={inputHandler}
     />
-    <Button type='submit' disabled={!formState.isValid}> SEND</Button>
-    </form></Card>)
+    <Button type='submit' disabled={!formState.isValid}>{isLoginMode ? 'LOGIN' : 'SIGNUP'}</Button>
+    </form>
+    <Button inverse onClick={switchModeHandler}>SWITCH TO {isLoginMode ? 'SUGNUP' : 'LOGIN'}</Button>
+    </Card>)
 }
 
 export default Auth;
