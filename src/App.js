2a83589a0be41df2030b23ae966be46b6d3c0b90
Serge Modin <modinsv@yandex.ru>
2020-04-05 10:32:27 +0300

    Added Auth Context for App-wide State

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState, useCallback} from "react";
 import {
   BrowserRouter as Router,
   Route,
@@ -11,10 +11,22 @@ import NewPlace from "./places/pages/NewPlace";
 import UserPlaces from "./places/pages/UserPlaces";
 import UpdatePlace from "./places/pages/UpdatePlace";
 import MainNavigation from "./shared/components/Navigation/MainNavigation";
+import {AuthContext} from './shared/context/auth-context'
 import Auth from './user/pages/Auth'
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
+
+  const login = useCallback(() => {
+    setIsLoggedIn(true)
+  }, [])
+
+  const logout = useCallback(() => {
+    setIsLoggedIn(false)
+  }, [])
+
   return (
+    <AuthContext.Provider value={{isLoggedIn : isLoggedIn, login: login, logout: logout}}>
     <Router>
       <MainNavigation />
       <main>
@@ -32,12 +44,13 @@ function App() {
             <UpdatePlace/>
           </Route>
           <Route path='/auth'>
-<Auth/>
+            <Auth/>
           </Route>
           <Redirect to='/'/>
         </Switch>
       </main>
     </Router>
+    </AuthContext.Provider>
   );
 }
 
